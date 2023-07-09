169f905199be78b964ba6b7d7e368cd236b36e7b
Tamacsa <tamacsa1998@gmail.com>
2023-07-09 16:24:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -59,8 +59,8 @@ app.post("/failure", function(request, response){
     response.redirect("/")
 });
 
-app.listen(3000, function(){
-    console.log("app is running on channel 3000")
+app.listen(process.env.PORT || 3000, function(){
+    console.log("app is running on channel")
 })
 //api key
 //abf49d8b43f03827bc487cd583cc2129-us21
